9566b2ecbe8fc923d50792010c1b14c144a6efbc
danielg-ppb <guilherme.daniel@flutter.com>
2024-10-18 21:41:50 +0100

    Add demo repo for project

---
+++ src/main/java/com/danielg/pulsar_man/PulsarManApplication.java
@@ -0,0 +1,13 @@
+package com.danielg.pulsar_man;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsarManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielg/pulsar_man/PulsarManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielg.pulsar_man;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
