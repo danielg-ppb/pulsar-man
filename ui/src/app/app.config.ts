ee6777fd97cc59542bafb28519ab4450cab2729a
danielg-ppb <guilherme.daniel@flutter.com>
2025-02-01 14:27:17 +0000

    Changes

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -4,10 +4,12 @@ import {provideRouter} from '@angular/router'
 import {routes} from './app.routes'
 import {provideClientHydration} from '@angular/platform-browser'
 import {provideHttpClient} from "@angular/common/http";
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import Aura from '@primeng/themes/aura';
 
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideAnimations()
+    providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideAnimations(),
+        /*providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })*/
     ],
 }
